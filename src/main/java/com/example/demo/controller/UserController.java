4dcde8138c14cb34ca5d6693244d8d14628937cd
SDUERYRG <202200300050@mail.sdu.edu.cn>
2025-05-06 22:22:40 +0800

    完成登录（wyc）

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,36 +6,84 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+import com.example.demo.utils.JwtUtil;
 import com.example.demo.utils.MD5;
 import com.example.demo.utils.Result;
 import com.example.demo.utils.ResultCode;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     @Resource
     private UserService userService;
+    // @PostMapping("/login")
+    // public String login(@RequestBody User user) {
+    //     // 这里应添加根据用户名和密码查询数据库的逻辑
+    //     // 示例中简单返回登录结果
+    //     if ("test".equals(user.getUsername()) && "test".equals(user.getPassword())) {
+    //         return "登录成功";
+    //     } else {
+    //         return "登录失败";
+    //     }
+    // }
+
+    /**
+     * 登录
+     * @param user 用户名和密码
+     * @return 登录结果
+     * @author wyc
+     * @date 5/6
+     */
     @PostMapping("/login")
-    public String login(@RequestBody User user) {
-        // 这里应添加根据用户名和密码查询数据库的逻辑
-        // 示例中简单返回登录结果
-        if ("test".equals(user.getUsername()) && "test".equals(user.getPassword())) {
-            return "登录成功";
-        } else {
-            return "登录失败";
+    @CrossOrigin
+    public Result login(@RequestBody User user, @RequestParam(required = false) HttpServletRequest request) {
+        System.out.println("开始登陆，user："+user);
+        User currentUser = userService.userAuthentication(user);
+        if (currentUser == null)
+            return Result.error(ResultCode.USER_LOGIN_ERROR.code(), ResultCode.USER_LOGIN_ERROR.message()); //账号或密码错误
+        //对数据库二次加密的密码解密
+        String jmPassword = MD5.md5MinusSalt(currentUser.getPassword());
+        System.out.println("数据库密码"+jmPassword);
+        //前端密码加密
+        String password = DigestUtils.md5Hex(user.getPassword());
+        System.out.println("前端密码"+password);
+        if(!jmPassword.equals(password)){
+            //密码错误
+            return Result.error(ResultCode.USER_LOGIN_ERROR.code(), ResultCode.USER_LOGIN_ERROR.message()); //账号或密码错误
         }
+        Map<String, Object> info = new HashMap<>();
+        info.put("username", currentUser.getUsername());
+        info.put("password", currentUser.getPassword());
+        //生成JWT字符串
+        String token = JwtUtil.sign(currentUser.getId().toString(), info);
+        if (request != null) {
+            request.setAttribute("username",currentUser.getUsername());
+        }
+
+        currentUser.setPassword(user.getPassword());
+        if (currentUser != null){
+            return Result.success(currentUser, token);  //登录成功，并返回token
+        }
+        else
+            return Result.error(ResultCode.ERROR.code(), "未知错误,登录失败(≧﹏ ≦)");
     }
+
+    
     @GetMapping("/hello")
     public User hello() {
         User user = new User();
